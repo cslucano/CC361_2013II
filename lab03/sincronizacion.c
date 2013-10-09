ff9c5acc47a8e93453151b5f16264a71f5c97c87
Carlos Salvatierra <cslucano@gmail.com>
2013-10-09 18:30:12 -0500

    agregamos texto a los problemas de sincronización

--- lab03/sincronizacion.c
+++ lab03/sincronizacion.c
@@ -1,3 +1,6 @@
+//
+// Imprimir A luego B
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
--- lab03/sincronizacionAAB.c
+++ lab03/sincronizacionAAB.c
@@ -1,3 +1,6 @@
+//
+// Imprimir A luego A luego B
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
